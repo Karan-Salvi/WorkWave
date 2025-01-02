0491421737daf967d4d7922cfe5bbcdf43b2dde1
Karan <karansalviwork@gmail.com>
2025-01-03 05:09:52 +0530

    Change needed for production

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({});
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URI}`
+      `${process.env.MONGO_URI}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 50000,
+        socketTimeoutMS: 45000,
+      }
     );
 
     if (!connectionInstance) {
