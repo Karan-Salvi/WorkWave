6cfd00c1a19bc7ce94206912d33b22f3897c9f59
Karan <karansalviwork@gmail.com>
2025-01-02 13:32:07 +0530

    Production changes

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,8 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => res.send("Server is running"));
+
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
