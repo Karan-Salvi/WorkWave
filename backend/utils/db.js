64aed35d8e1f59c3bb92ee3446c055ebe3e641c9
Karan <karansalviwork@gmail.com>
2025-01-03 04:55:59 +0530

    Fixing Server Timeout

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("mongodb connected successfully");
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}`
+    );
+
+    if (!connectionInstance) {
+      console.log("MongoDB connection failed");
+    }
+    console.log(
+      "MongoDB connected Successfully on server : " +
+        connectionInstance.connection.host
+    );
   } catch (error) {
-    console.log(error);
+    console.log("MongoDB connection failed due to some error :", error);
   }
 };
 module.exports = connectDB;
