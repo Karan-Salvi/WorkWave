bd77846a414870eb2c34526fcc9346de0c8722ae
Karan <karansalviwork@gmail.com>
2025-01-06 21:22:13 +0530

    Fixing issues in the production

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,13 +5,7 @@ dotenv.config({});
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URI}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 50000,
-        socketTimeoutMS: 45000,
-      }
+      `${process.env.MONGO_URI}`
     );
 
     if (!connectionInstance) {
